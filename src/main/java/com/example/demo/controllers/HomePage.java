400205e10234eb901ea7d9844a7e65bc94f08c18
saraems <sara.ems.94@gmail.com>
2021-12-28 17:04:51 +0100

    RFID-SYSTEM-4-push-imports

--- src/main/java/com/example/demo/controllers/HomePage.java
+++ src/main/java/com/example/demo/controllers/HomePage.java
@@ -2,6 +2,9 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import newSDK.conncet.library.com.payne.reader.Reader;
+import newSDK.conncet.library.com.payne.reader.bean.config.AntennaCount;
+import newSDK.conncet.library.com.payne.reader.process.ReaderImpl;
 
 @RestController
 public class HomePage {
@@ -9,6 +12,8 @@ public class HomePage {
 
     @GetMapping("/")
     public String sayHello() {
+        Reader mReader = ReaderImpl.create(AntennaCount.SIXTEEN_CHANNELS);
         return  HELLO;
     }
+
 }
